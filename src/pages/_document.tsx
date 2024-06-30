9117500b5f6236770633e3402d03e6d680ce63c7
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2024-06-30 23:01:07 +0600

    added umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Head, Html, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
 	return (
 		<Html lang="en">
 			<Head />
+			<Script
+				src="https://umami-m4wg0kc.shahriyar.dev/script.js"
+				type="text/javascript"
+				data-website-id="6262683a-a600-47ee-9357-37728d634fda"
+			/>
 			<body>
 				<Main />
 				<NextScript />
