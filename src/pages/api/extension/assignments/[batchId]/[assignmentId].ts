cd67b53b26805e2703e2134bc89c0b43dc6775ba
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-05-22 00:48:48 +0600

    updated route

--- src/pages/api/extension/assignments/[batchId]/[assignmentId].ts
+++ src/pages/api/extension/assignments/[batchId]/[assignmentId].ts
@@ -5,10 +5,11 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
 ) {
-	const { assignmentId } = req.query
+	const { batchId, assignmentId } = req.query
 	const jsons = await db.jsonData.findMany({
 		where: {
 			assignmentId: assignmentId as string,
+			batchId: batchId as string,
 		},
 		include: {
 			assignment: true,
