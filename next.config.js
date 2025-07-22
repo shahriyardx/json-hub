70cc91ca517d23a2b83689d6dd419fcb269e19ec
shahriyardx <mdshahriyaralam552@gmail.com>
2025-07-23 00:40:15 +0600

    remove filter to return all jsons

--- next.config.js
+++ next.config.js
@@ -1,53 +1,54 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js")
-
-/** @type {import("next").NextConfig} */
-const config = {
-	async headers() {
-		return [
-			{
-				// matching all API routes
-				source: "/api/:path*",
-				headers: [
-					{ key: "Access-Control-Allow-Origin", value: "*" },
-					{
-						key: "Access-Control-Allow-Methods",
-						value: "*",
-					},
-					{
-						key: "Access-Control-Allow-Headers",
-						value: "*",
-					},
-				],
-			},
-		]
-	},
-	reactStrictMode: false,
-	modularizeImports: {
-		"@icons-pack/react-simple-icons": {
-			transform: "@icons-pack/react-simple-icons/{{member}}",
-		},
-	},
-	/**
-	 * If you are using `appDir` then you must comment the below `i18n` config out.
-	 *
-	 * @see https://github.com/vercel/next.js/issues/41980
-	 */
-	i18n: {
-		locales: ["en"],
-		defaultLocale: "en",
-	},
-	transpilePackages: ["geist"],
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	output: "standalone",
-}
-
-export default config
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js")
+
+/** @type {import("next").NextConfig} */
+const config = {
+	async headers() {
+		return [
+			{
+				// matching all API routes
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "*",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "*",
+					},
+				],
+			},
+		]
+	},
+	reactStrictMode: false,
+	modularizeImports: {
+		"@icons-pack/react-simple-icons": {
+			transform: "@icons-pack/react-simple-icons/{{member}}",
+		},
+	},
+	/**
+	 * If you are using `appDir` then you must comment the below `i18n` config out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	transpilePackages: ["geist"],
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	output: "standalone",
+}
+
+export default config;
+
